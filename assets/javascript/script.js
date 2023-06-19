17cbf4b73d6fc6332c18708b7451f447905f6b0e
fquaranta <fquaranta00@hotmail.com>
2023-06-18 23:18:59 -0300

    primer carga tercerPreEntrega

---
+++ assets/javascript/script.js
@@ -0,0 +1,49 @@
+// Funcion para guardar datos de pacientes en local storage
+function guardarPaciente(paciente) {
+    var pacientes = JSON.parse(localStorage.getItem('pacientes')) || [];
+    pacientes.push(paciente);
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+}
+
+let formulario = document.getElementById('formulario-paciente');
+let elementosFormulario = document.querySelectorAll('#formulario-paciente input');
+
+// Recorrer los elementos y vacía los placeholders de los campos del formulario
+elementosFormulario.forEach(function(elemento) {
+    elemento.addEventListener('focus', function() {
+        this.setAttribute('placeholder', '');
+    });
+});
+
+//cuando le doy guardar al formulario cargado, toma los valores de cada campo y los guarda en el objeto
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Obtener los valores de los campos
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var documento = document.getElementById('documento').value;
+    var obraSocial = document.getElementById('obraSocial').value;
+    var activo = document.getElementById('activo').value;
+
+    // Crear un objeto paciente con los datos
+    var nuevoPaciente = {
+        nombre: nombre,
+        apellido: apellido,
+        documento: documento,
+        obraSocial: obraSocial,
+        activo: activo
+    };
+
+
+   // Ejecuta la funcion y guarda el objeto paciente en el Local Storage
+    guardarPaciente(nuevoPaciente);
+
+    // Devuelve ensaje de éxito
+    alert('Paciente registrado correctamente');
+    // window.location.href = 'otra_pagina.html';
+
+    // Restablecer el formulario a su estado inicial
+    document.getElementById('formulario-paciente').reset();
+
+
+});
---
+++ assets/javascript/script2.js
@@ -0,0 +1,22 @@
+let pacientes = JSON.parse(localStorage.getItem('pacientes'));
+
+// Obtener el contenedor de resultados
+let resultadosContainer = document.getElementById('resultados-pacientes');
+
+// Recorrer los pacientes y crear un div para cada uno
+pacientes.forEach(function (paciente) {
+    // Crear el div del paciente
+    var pacienteDiv = document.createElement('div');
+    pacienteDiv.classList.add('paciente');
+
+    // Agregar el contenido del paciente al div
+    pacienteDiv.innerHTML = `
+        <h3>${paciente.nombre} ${paciente.apellido}</h3>
+        <p>Documento: ${paciente.documento}</p>
+        <p>Obra Social: ${paciente.obraSocial}</p>
+        <p>Activo: ${paciente.activo}</p>
+      `;
+
+    // Agregar el div del paciente al contenedor de resultados
+    resultadosContainer.appendChild(pacienteDiv);
+});
